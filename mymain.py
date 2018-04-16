3db8d052e0695f1fb8f096444f7076646bbd2510
Koomi <koomiadeh@gmail.com>
2018-04-17 00:55:39 +0200

    new

--- mymain.py
+++ mymain.py
@@ -28,14 +28,13 @@ def confirm_signup():
 		#errors["username"] = "The '{0}' have not to be empty and has no space.The length has not to be out of the range 3 to 21".format("username")
 		ur="The '{0}' have not to be empty and has no space.The length has not to be out of the range 3 to 21".format("username")
 		u_error=ur
-		errors_massage.append(ur)
-		#return errors, errors_massage
+		#errors_massage.append(ur)
+		return u_error
 	else:
-		errors["username"]=""
+		#errors["username"]=""
 		ur=""
 		u_error=ur
-		errors_massage.append(ur)
-		return errors, errors_massage
+		return u_error
 
 	if len(password) not in range(3, 21) or password.find(' ')!=-1:
 		#errors["password"] = "The '{0}'length has not to be out of the range 3 to 21 and '{1}' must match the given '{2}'".format("password", "verify_password", "password")
@@ -54,7 +53,7 @@ def confirm_signup():
 		pv_error=pvr
 		return pv_error
 	else:
-		errors["verify_password"] = ""
+		#errors["verify_password"] = ""
 		pvr=""
 		pv_error=pvr
 		return pv_error
@@ -67,16 +66,15 @@ def confirm_signup():
                   ### case the email is not provided or the email ist left empty
 	else:
 		if len(email)==0:
-			errors['email']==''
+			#errors['email']==''
 			emr=""
 			em_error=emr
 			return em_error
 
 	if len(u_error)==0 and len(p_error)==0 and len(pv_error)==0 and len(em_error)==0:
-		###if errors=={ "username": "", "password": "", "verify_password": "", "email" : ""}: # case no errors == successful signed up
 		return render_template('confirm.html', email=email, username=username)
+
 		#case errors == at least field has errors
 	else:
 		return render_template('confirmss.html',u_error=u_error, p_error=p_error, pv_error=pv_error, em_error=em_error,username=username, email=email)
-		#return render_template('confirm.html',u_error=u_error,p_error=p_error,pv_error=pv_error,em_error=em_error, email=email, username=username)
 app.run()
